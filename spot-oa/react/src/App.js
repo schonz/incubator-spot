23a2048c62cf456cc0c3906ed11fa6c4dee3f123
Mark Schoeni <markschoeni@gmail.com>
2018-10-24 17:38:06 -0500

    Moving Files over and Adding Imports
    - Removed redirct from index.html
    - commented all Node-Modules libs from html pages
    - moved over js folders and files from ui to src folder in react (All but cubehelix)
    - added jQuery import to suspicious.js
    - fixed location to window.location
    - Still broken

--- spot-oa/react/src/App.js
+++ spot-oa/react/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import FlowPage from './pages/flow/suspicious'
+
 class App extends Component {
   render() {
     return (
@@ -20,6 +22,9 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <body>
+
+        </body>
       </div>
     );
   }
